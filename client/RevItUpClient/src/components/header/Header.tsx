1d2a7e9ecc351abced252e5f641fc57b7c168fb5
UnkindlyBiased <inoutoftheverge@gmail.com>
2024-02-07 16:53:43 +0200

    Start of the main page

--- client/RevItUpClient/src/components/header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import './styles/Header.module.css'
-
-export function Header() {
-    return (
-        <header>
-            <img src='./REVITUP.png' alt=''/>
-        </header>
-    )
-}
---
+++ client/revitup-client/src/components/ui/header/Header.jsx
@@ -0,0 +1,9 @@
+import './styles/Header.module.css'
+
+export default function Header() {
+    return(
+        <header>
+            <img src='./REVITUP.png' alt='REVITUP'/>
+        </header>
+    )
+}
